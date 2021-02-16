c2556971147257688e1b824933a0af7f0d387979
Ludvík Prokopec <39367469+ujikCz@users.noreply.github.com>
2021-02-16 23:09:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,92 +1,95 @@
-class parallelThread {
 
-    constructor(workerAnonymousFunction, ...args) {
+(function (global, factory) {
+   function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-        if (!Worker in window) throw Error(``);
+  (typeof exports === "undefined" ? "undefined" : _typeof(exports)) === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : (global = global || self, global.ParallelThread = factory());
+})(this, function () {
+  "use strict";
 
-        function callWorkerInvoker(fn, ...args) {
+  function isFunc(func) {
+    return typeof func === 'function';
+  }
 
-            this.send = function(msg) {
+  function ParallelThread(workerAnonymousFunction) {
+    if (!Worker in window) throw Error("ParallelThread(...) Browser need to support Web Workers");
+    if (!isFunc(workerAnonymousFunction)) throw TypeError("ParallelThread(...) first parameter must be function");
 
-                let isFunction = false;
-                if(typeof msg === 'function') {
+    function callWorkerInvoker(fn) {
+      this.send = function (msg) {
+        var isFunction = false;
 
-                    msg = msg.toString();
-                    isFunction = true; 
-
-                }
-
-                this.postMessage({
-                    isFunction,
-                    msg
-                });
-
-            }
-
-            console.log(this);
-
-            this.get = function(callback) {
-
-                this.onmessage = (msg) => {
-
-                    if(msg.data.isFunction) {
-                        msg.data.msg = new Function(`return (${ msg.data.msg })`)(msg.data.msg);
-                    }
-        
-                    callback(msg.data.msg, msg);
-
-                }
-
-            }
-
-            fn(...args);
-
+        if (isFunc(msg)) {
+          msg = msg.toString();
+          isFunction = true;
         }
 
-        var blobURL = URL.createObjectURL(new Blob([`(${ (callWorkerInvoker, fn, ...args) => callWorkerInvoker(fn, ...args) })(${ callWorkerInvoker }, ${ workerAnonymousFunction }, ${ args })`], {
-            type: 'application/javascript'
-        }));
-
-        this.thread = new Worker(blobURL);
-
-        return this;
-
-    }
-
-    disconnect() {
-
-        this.thread.terminate();
-
-    }
-
-    send(msg) {
-
-        let isFunction = false;
-        if(typeof msg === 'function') {
-
-            msg = msg.toString();
-            isFunction = true; 
-
-        }
-
-        this.thread.postMessage({
-            isFunction,
-            msg
+        this.postMessage({
+          isFunction: isFunction,
+          msg: msg
         });
+      };
 
-    }
+      this.get = function (callback) {
+        this.onmessage = function (msg) {
+          if (msg.data.isFunction) {
+            msg.data.msg = new Function("return (".concat(msg.data.msg, ")"))(msg.data.msg);
+          }
 
-    get(callback) {
-
-        this.thread.onmessage = (msg) => {
-
-            if(msg.data.isFunction) {
-                msg.data.msg = new Function(`return (${ msg.data.msg })`)(msg.data.msg);
-            }
-
-            callback(msg.data.msg, msg);
+          callback(msg.data.msg, msg);
         };
+      };
 
+      for (var _len2 = arguments.length, args = new Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+        args[_key2 - 1] = arguments[_key2];
+      }
+
+      fn.apply(void 0, args);
     }
 
-}
+    for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+      args[_key - 1] = arguments[_key];
+    }
+
+    var blobURL = URL.createObjectURL(new Blob(["(".concat(function (callWorkerInvoker, fn) {
+      for (var _len3 = arguments.length, args = new Array(_len3 > 2 ? _len3 - 2 : 0), _key3 = 2; _key3 < _len3; _key3++) {
+        args[_key3 - 2] = arguments[_key3];
+      }
+
+      return callWorkerInvoker.apply(void 0, [fn].concat(args));
+    }, ")(").concat(callWorkerInvoker, ", ").concat(workerAnonymousFunction, ", ").concat(args, ")")], {
+      type: 'application/javascript'
+    }));
+    this.thread = new Worker(blobURL);
+    return this;
+  }
+
+  ParallelThread.prototype.disconnect = function () {
+    this.thread.terminate();
+  };
+
+  ParallelThread.prototype.send = function (msg) {
+    var isFunction = false;
+
+    if (isFunc(msg)) {
+      msg = msg.toString();
+      isFunction = true;
+    }
+
+    this.thread.postMessage({
+      isFunction: isFunction,
+      msg: msg
+    });
+  };
+
+  ParallelThread.prototype.get = function (callback) {
+    this.thread.onmessage = function (msg) {
+      if (msg.data.isFunction) {
+        msg.data.msg = new Function("return (".concat(msg.data.msg, ")"))(msg.data.msg);
+      }
+
+      callback(msg.data.msg, msg);
+    };
+  };
+
+  return ParallelThread;
+});
